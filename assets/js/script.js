42b67aa7e0d9c56c65cc0268f19599b56ff24c09
Niccola Winter <niccola.winter@students.codeinstitute.net>
2024-10-12 10:59:19 +0000

    display additonal quesiton to user

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,3 @@
-// Wait for DOM to finish loading before running game//
-//Get button elements and add event listners//
-
 // Wait for the DOM to finish loading before running the game
 // Get the button elements and add event listeners to them
 
@@ -13,41 +10,62 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert("You clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         });
     }
+
+    runGame("addition");
+
 });
 
-
 /**
- * The main game 'loop', called when the script is first loaded
- * and after the users answers has been processed
+ * The main game "loop", called when the script is first loaded
+ * and after the user's answer has been processed
  */
+function runGame(gameType) {
 
-function runGame (){
-    //Creates two ramdom numbers between 1 and 25//
-let num1= Math.floor(Math.random() * 25) + 1;
-let num2= Math.floor(Math.random() * 25) + 1;
+    // Creates two random numbers between 1 and 25
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
+
 }
+
 function checkAnswer() {
 
 }
-function calculateCorrectAnswer () {
+
+function calculateCorrectAnswer() {
 
 }
-function incrementScore (){
+
+function incrementScore() {
 
 }
-function incrementWrongAnswer (){
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion(operand1, operand2) {
+
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
     
 }
-function displayAdditionQuestion (){
-    
+
+function displaySubtractQuestion() {
+
 }
-function displaySubtractQuestion (){
-    
-}
-function displayMultiplyQuestion (){
+
+function displayMultiplyQuestion() {
     
 }
